49d0d2038cb87cce68099016e01eac3d2e219605
Jonathan Catalano <Jonmacphers@gmail.com>
2024-12-24 12:30:54 -0500

    Update agent-automatic-setup.ps1

--- windows/agent-automatic-setup.ps1
+++ windows/agent-automatic-setup.ps1
@@ -18,8 +18,17 @@ function Uninstall-WazuhAgent {
     # Stop the Wazuh service
     Stop-Service -Name "WazuhSvc" -ErrorAction SilentlyContinue
 
+    # Wait a bit to ensure the service stops
+    Start-Sleep -Seconds 5
+
+    # Kill any leftover Wazuh processes
+    Get-Process -Name "WazuhSvc" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+
     # Uninstall the Wazuh agent using msiexec
-    msiexec.exe /x $env:tmp\wazuh-agent.msi /q 2>$null
+    msiexec.exe /x $env:tmp\wazuh-agent.msi /quiet /norestart
+
+    # Ensure the uninstallation is complete
+    Start-Sleep -Seconds 10
 
     # Remove the Wazuh agent installation directory
     Remove-Item -Recurse -Force $ossecAgentPath -ErrorAction SilentlyContinue
